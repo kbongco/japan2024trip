063ba6ef40adc4363b84286d95f46558967ef04b
Kathleen B <kathleen.bongco@gmail.com>
2025-01-12 23:13:30 -0500

    nav bar mobile completed

--- src/Components/NavBar/NavBar.tsx
+++ src/Components/NavBar/NavBar.tsx
@@ -9,6 +9,11 @@ export default function NavBar() {
       <div className='chbi-jpn-nav'>
         <p className='chbi-nav-text'>Chibi Japan Viz</p>
         <FontAwesomeIcon icon={faBars} />
+        <div className='chbi-nav-text-display'>
+        <p className='chbi-nav-text-desktop'>Stats</p>
+        <p className='chbi-nav-text-desktop'>Food</p>
+        <p className='chbi-nav-text-desktop'>Transport</p>
+         </div>
       </div>
     </>
   )
