318cf039a3ba246e3f3d081ffb5219ee1f6ef23f
Kathleen B <kathleen.bongco@gmail.com>
2025-02-03 14:38:53 -0500

    funstuff json added

---
+++ src/data/funstuff.ts
@@ -0,0 +1,37 @@
+export const funStuff = [
+  {
+    id: "funStuff_001",
+    name: "Arcade Spending",
+    description: "21% of the 67261 came from arcade spending. That's a lot of dance rush and rhythm games you played there!"
+  },
+  {
+    id: "funStuff_002",
+    name: "Plushies Won",
+    description: "You spent about 11300 JPY on trying to win plushies! Lucky for you, you won 7! But at what cost? "
+  },
+  {
+    id: "funStuff_003",
+    name: "Karaoke",
+    description: "You ticked off your karaoke dreams and went to Karaokekan and sang “Bakamitai” amongst other songs.  This set you back 5000 JPY. And you had unlimited drinks!"
+  },
+  {
+    id: "funStuff_004",
+    name: "Lifting things",
+    description: "You couldn't stay away, couldn't you? Your coach somehow talked you into training this vacation, and your teammate told you about Oni gym. You spent 5500 JPY for a week day pass. As much as you said you didn't want to,  you enjoyed it and found confidence with heavier weight!"
+  }, 
+  {
+    id: "funStuff_005",
+    name: "Disapplointment",
+    description: "You heard about Nintendo world, and unfortunately it fell through. You spent 12000 JPY to go to Universal Studios Osaka. Unfortunately, the only thing you did there was saw a show and eat some overpriced Harry potter food. Maybe next time you'll actually wake up early so you can get Express pass. But hey, at least you went karaoking!"
+  },
+  {
+    id: "funStuff_006",
+    name: "A haircut",
+    description: "Your hair was getting a little long, and you wanted to retouch your highlights. You sat on a chair for 4 hours and got a taste of Japanese hospitality, your haircut included a massage too! This set you back 28000 JPY. You might as well have waited until you got back home to get a hair cut, but your stylist got your hair close to what you get at home!"
+  },
+  {
+    id: "funStuff_007",
+    name: "Nintendo Museum",
+    description: "You went to Kyoto! The place where Nintendo is located. Your buddy entered the lottery and got you a ticket to go to the nintendo museum. No Speed run tricks! "
+  }
+]
