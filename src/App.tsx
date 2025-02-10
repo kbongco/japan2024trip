4518de486a0e0f44c37da8b728965dd3f49c3d38
Kathleen B <kathleen.bongco@gmail.com>
2025-02-10 11:50:40 -0500

    pics

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css'
 import BasicStats from './Components/BasicStats/BasicStats'
+import FoodTimeLine from './Components/FoodTimeLine/Food'
 import FunStuff from './Components/FunStuff/FunStuff'
 import NavBar from './Components/NavBar/NavBar'
 import Transportation from './Components/Transportation/Transportation'
@@ -14,6 +15,7 @@ function App() {
         <Welcome />
         <BasicStats />
         <Transportation />
+        <FoodTimeLine/>
         <FunStuff/>
       </div>
     </>
